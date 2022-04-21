7a23db496f20246bcd9b54f469617147a81dfb56
cheuhxg <3193079152@qq.com>
2022-04-21 10:40:17 +0800

    redefine unreachable edge->INF(0x3fffffff)

--- adjacencymatrix.cpp
+++ adjacencymatrix.cpp
@@ -6,7 +6,7 @@ namespace myalgorithm {
 
 AdjacencyMatrix::AdjacencyMatrix(const size_t node_size)
 	: node_size_(node_size),
-		graph_(node_size, std::vector<int>(node_size, 0)) {}
+		graph_(node_size, std::vector<int>(node_size, 0x3fffffff)) {}
 
 bool AdjacencyMatrix::UpdateEdge(const size_t node_out,
 															const size_t node_in, 
@@ -23,11 +23,11 @@ bool AdjacencyMatrix::UpdateEdge(const size_t node_out,
 bool AdjacencyMatrix::DeleteEdge(const size_t node_out,
 																 const size_t node_in) {
 	if (node_out >= node_size_ || node_in >= node_size_ ||
-			graph_[node_out][node_in] == 0) {
+			graph_[node_out][node_in] == 0x3fffffff) {
 		return false;
 	}
 
-	graph_[node_out][node_in] = 0;
+	graph_[node_out][node_in] = 0x3fffffff;
 	return true;
 }
 
@@ -40,7 +40,10 @@ void AdjacencyMatrix::Show() const {
 	for (int i = 0; i < node_size_; ++i) {
 		std::cout << i << ":\t";
 		for (const auto& weight : graph_[i]) {
-			std::cout << weight << "\t";
+			if(weight == 0x3fffffff)
+				std::cout << "INF" << "\t";
+			else
+				std::cout << weight << "\t";
 		}
 		std::cout << std::endl;
 	}
@@ -62,7 +65,7 @@ void AdjacencyMatrix::DFS(const size_t start_vertex,
 	ret_vertices.push_back(start_vertex);
 
 	for (size_t i = 0; i < node_size_; ++i) {
-		if (graph_[start_vertex][i] > 0 && !is_visited[i]) {
+		if (graph_[start_vertex][i] != 0x3fffffff && !is_visited[i]) {
 			DFS(i, ret_vertices, is_visited);
 		}
 	}
@@ -86,7 +89,7 @@ void AdjacencyMatrix::BFS(const size_t start_vertex,
 			ret_vertices.push_back(vertex);
 
 			for (int i = 0; i < node_size_; ++i) {
-				if (graph_[vertex][i] > 0 && !is_visited[i]) {
+				if (graph_[vertex][i] != 0x3fffffff && !is_visited[i]) {
 					is_visited[i] = true;
 					queue_vertex.push(i);
 				}
@@ -117,7 +120,7 @@ void AdjacencyMatrix::Dijkstra(const size_t start_vertex,
 		is_visited[min_vertex] = true;
 		path.push_back(min_vertex);
 		for (int j = 0; j < node_size_; ++j) {
-			if (!is_visited[j] && graph_[min_vertex][j] > 0 &&
+			if (!is_visited[j] && graph_[min_vertex][j] != 0x3fffffff &&
 				graph_[min_vertex][j] + distance[min_vertex] < distance[j]) {
 				distance[j] = graph_[min_vertex][j] + distance[min_vertex];
 			}
