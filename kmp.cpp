0e66e6ad10ee3709c05751ced85eeb524e4e40db
cheuhxg <3193079152@qq.com>
2022-06-17 12:32:08 +0800

    add kmp class

---
+++ kmp.cpp
@@ -0,0 +1,5 @@
+#include "kmp.h"
+
+namespace myalgorithm {
+
+}
---
+++ kmp.h
@@ -0,0 +1,17 @@
+#ifndef KMP_H_
+#define KMP_H_
+
+#include <vector>
+#include <random>
+
+namespace myalgorithm {
+
+class Kmp {
+private:
+public:
+
+};	//	class Kmp
+
+}	//	namespace myalgorithm
+
+#endif
